a584281202547b56e523be3490df20b76efced43
“Sofiia <golets2015@gmail.com>
2026-08-13 01:48:35 +0300

    My new commit

--- tests/unit10.spec.ts
+++ tests/unit10.spec.ts
@@ -15,7 +15,7 @@ test('Login with valid credentials', async ({ page }) => {
     await expect(page).toHaveURL('https://practicesoftwaretesting.com/account');
 
     //Check for the page title to be "My Account"
-    await expect(page.getByRole('heading', { name: 'My account' })).toBeVisible();
+    await expect(page.locator('[data-test="page-title"]')).toHaveText('My account');
 
     //Check username in the navigation bar to be "Jane Doe"
     await expect(page.locator('[data-test="nav-menu"]')).toHaveText('Jane Doe');
