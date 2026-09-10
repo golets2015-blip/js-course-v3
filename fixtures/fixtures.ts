427aed8e30b2b415fc89be1556ca259c40e9cd13
“Sofiia <golets2015@gmail.com>
2026-09-11 01:35:34 +0300

    Unit 14

--- fixtures/fixtures.ts
+++ fixtures/fixtures.ts
@@ -3,8 +3,11 @@ import { AllPages } from '../page objects/allPages.page';
 
 type MyFixtures = {
     app: AllPages;
-    loggedInApp:AllPages;
-}
+    loggedInApp: AllPages;
+};
+type LoginResponse = {
+    access_token: string;
+};
 
 export const test = base.extend<MyFixtures>({
     app: async ({ page }, use) => {
@@ -12,17 +15,34 @@ export const test = base.extend<MyFixtures>({
 
         await use(allPages);
     },
-    loggedInApp: async ({ browser }, use) => {
-        const context = await browser.newContext({
-        storageState: 'playwright/.auth/user.json'
-    });
+    loggedInApp: async ({ browser, request }, use) => {
+        const context = await browser.newContext();
+        const resp = await request.post(
+            'https://api.practicesoftwaretesting.com/users/login',
+            {
+                data: {
+                    email: 'customer2@practicesoftwaretesting.com',
+                    password: 'welcome01'
+                }
+            }
+        );
+        const jsonData = await resp.json() as LoginResponse;
+        const token = jsonData.access_token;
 
-    const page = await context.newPage();
-    const allPages = new AllPages(page);
+        const page = await context.newPage();
+        const allPages = new AllPages(page);
 
-    await page.goto('/account');
+        await page.goto('/');
 
-    await use(allPages);
-    await context.close();
+        await page.evaluate((token) => {
+            localStorage.setItem('auth-token', token);
+        }, token);
+
+        await page.reload();
+
+        await page.goto('/account');
+
+        await use(allPages);
+        await context.close();
     }
 })
